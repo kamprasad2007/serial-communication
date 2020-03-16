681cfba5c02aff73905852392a16f338c84596e1
Manoj Prasad <kamprasad2007@gmail.com>
2020-03-16 17:40:45 +0800

    first commit

---
+++ Serial/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Serial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmdPort = new System.Windows.Forms.ComboBox();
+            this.cmbRate = new System.Windows.Forms.ComboBox();
+            this.prbStatus = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtDelay = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDelay)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmdPort
+            // 
+            this.cmdPort.FormattingEnabled = true;
+            this.cmdPort.Location = new System.Drawing.Point(57, 18);
+            this.cmdPort.Name = "cmdPort";
+            this.cmdPort.Size = new System.Drawing.Size(121, 21);
+            this.cmdPort.TabIndex = 0;
+            // 
+            // cmbRate
+            // 
+            this.cmbRate.FormattingEnabled = true;
+            this.cmbRate.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.cmbRate.Location = new System.Drawing.Point(57, 45);
+            this.cmbRate.Name = "cmbRate";
+            this.cmbRate.Size = new System.Drawing.Size(121, 21);
+            this.cmbRate.TabIndex = 1;
+            // 
+            // prbStatus
+            // 
+            this.prbStatus.Location = new System.Drawing.Point(283, 16);
+            this.prbStatus.Name = "prbStatus";
+            this.prbStatus.Size = new System.Drawing.Size(125, 23);
+            this.prbStatus.Step = 1;
+            this.prbStatus.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.prbStatus);
+            this.groupBox1.Controls.Add(this.txtMessage);
+            this.groupBox1.Controls.Add(this.txtDelay);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 381);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Send Here";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(357, 59);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Ctr+Enter";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(152, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Sec";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(231, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Progress";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Message :";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Enabled = false;
+            this.txtMessage.Location = new System.Drawing.Point(9, 75);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessage.Size = new System.Drawing.Size(399, 300);
+            this.txtMessage.TabIndex = 0;
+            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyDown);
+            // 
+            // txtDelay
+            // 
+            this.txtDelay.Enabled = false;
+            this.txtDelay.Location = new System.Drawing.Point(57, 19);
+            this.txtDelay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtDelay.Name = "txtDelay";
+            this.txtDelay.Size = new System.Drawing.Size(89, 20);
+            this.txtDelay.TabIndex = 3;
+            this.txtDelay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Delay";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtOutput);
+            this.groupBox2.Location = new System.Drawing.Point(432, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(414, 467);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Received Here";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(6, 19);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(402, 442);
+            this.txtOutput.TabIndex = 0;
+            this.txtOutput.Text = "";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(184, 17);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(95, 21);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Rate";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cmdPort);
+            this.groupBox3.Controls.Add(this.cmbRate);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.btnConnect);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(414, 80);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 482);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Serial Communication";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDelay)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmdPort;
+        private System.Windows.Forms.ComboBox cmbRate;
+        private System.Windows.Forms.ProgressBar prbStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown txtDelay;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Serial/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Serial
+{
+    public partial class Form1 : Form
+    {
+        private Thread processThread;
+        private Thread sendingThread;
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailablePorts();
+        }
+        void getAvailablePorts()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            cmdPort.Items.AddRange(ports);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                    prbStatus.Value = 0;
+                    btnConnect.Text = "Connect";
+                    txtMessage.Enabled = false;
+                    txtDelay.Enabled = false;
+                    txtMessage.Enabled = false;
+                    processThread.Abort();
+                    sendingThread.Abort();
+
+                    txtMessage.Clear();
+                    txtOutput.Clear();
+                }
+                else
+                {
+                    if (cmdPort.Text == "" || cmbRate.Text == "")
+                    {
+                        txtOutput.Text = "Please select port settings";
+                    }
+                    else
+                    {
+                        serialPort.PortName = cmdPort.Text;
+                        serialPort.BaudRate = Convert.ToInt32(cmbRate.Text);
+                        serialPort.Open();
+                        txtMessage.Enabled = true;
+                        txtDelay.Enabled = true;
+                        txtMessage.Enabled = true;
+                        btnConnect.Text = "Disconnect";
+                        processThread = new Thread(() =>
+                        {
+                            while (true)
+                            {
+                                if (serialPort.IsOpen)
+                                {
+                                    try
+                                    {
+                                        string value = serialPort.ReadLine();
+                                        if (value != null)
+                                        {
+                                            this.Invoke((Action)delegate ()
+                                            {
+                                                txtOutput.AppendText(value);
+                                            });
+                                        }
+                                    }
+                                    catch (TimeoutException)
+                                    {
+                                        txtOutput.AppendText("Timeout Exception");
+                                    }
+                                }
+                                Thread.Sleep(1500);
+                            }
+                        });
+                        processThread.Start();
+                    }
+                }
+               
+            }
+            catch(UnauthorizedAccessException)
+            {
+                txtOutput.Text = "Unauthorized Access";
+
+            }
+        }
+
+        private void txtMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyData == (Keys.Control | Keys.Enter)))
+            {
+                int lines = txtMessage.Lines.Length;
+                int second = Int32.Parse(txtDelay.Text);
+                prbStatus.Maximum = lines;
+
+                sendingThread = new Thread(() =>
+                {
+                    for (int i = 0; i < lines; i++)
+                    {
+                        serialPort.WriteLine(txtMessage.Lines[i]);
+                        this.Invoke((Action)delegate ()
+                        {
+                            prbStatus.Value = i+1;
+                        });
+                        Thread.Sleep(second * 1000);
+                    }
+
+                    this.Invoke((Action)delegate ()
+                    {
+                        prbStatus.Value = 0;
+                        txtMessage.Text = "";
+                        txtMessage.Enabled = true;
+                    });
+                  
+                });
+                sendingThread.Start();
+                txtMessage.Enabled = false;
+            }
+        }
+    }
+}
